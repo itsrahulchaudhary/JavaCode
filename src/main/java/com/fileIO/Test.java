2877d50e36f82ccb0c43f4a9c7242b91c170ebcf
itsrahulchaudhary <rahulcy49@gmail.com>
2021-03-29 16:17:31 +0530

    add

--- src/main/java/com/fileIO/Test.java
+++ src/main/java/com/fileIO/Test.java
@@ -1,13 +1,27 @@
 package com.fileIO;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Test {
 	public static void main(String[] args) throws IOException {
-		File f = new File("abc20012021.txt");
-		f.createNewFile();
-		System.out.println(f.exists());
+		/*
+		 * File f = new File("zzzz.txt"); f.createNewFile();
+		 * System.out.println(f.exists());
+		 */
+		FileWriter fw = new FileWriter("fileIONote.txt");
+		BufferedWriter bw = new BufferedWriter(fw);
+		bw.write(114);
+		bw.write("ahul\nChadhary");
+		bw.newLine();
+		char ch[] = { 'a', 'b', 'c' };
+		bw.write(ch);
+		bw.newLine();
+		bw.flush();
+		bw.close();
+		
 	}
 
 }
